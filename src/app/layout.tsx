5e892c646fc395d0fba9e652343bbd0842e5938f
fabian-tally-it <fabian@tally.legal>
2024-12-14 13:50:53 -0500

    remove google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { GoogleAnalytics } from "@next/third-parties/google";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -28,7 +27,6 @@ export default async function RootLayout({
 
         {children}
       </body>
-      <GoogleAnalytics gaId="G-YYBXWKDRM0" />
     </html>
   );
 }
