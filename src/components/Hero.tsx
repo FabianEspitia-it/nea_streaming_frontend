33edb03400e6b22f99a5f841aedeb6d090e06fd6
FabianEspitia-it <fabianespitia370@gmail.com>
2025-06-26 21:19:06 -0500

    add prime video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -29,6 +29,12 @@ export default function Hero() {
       href: "/new_session",
       id: 6,
     },
+
+    {
+      label: "Código de inicio de sesión (PRIME VIDEO)",
+      href: "/session_prime",
+      id: 7,
+    },
   ];
 
   return (
