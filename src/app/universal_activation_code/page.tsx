a006be0f0f1084a5fdaa60e5b1304a77aa5b0c18
Fabian Espitia <fabianespitia370@gmail.com>
2026-05-01 03:33:15 -0500

    fix: update placeholder email in UniversalActivationCode component

--- src/app/universal_activation_code/page.tsx
+++ src/app/universal_activation_code/page.tsx
@@ -98,7 +98,7 @@ export default function UniversalActivationCode() {
               <input
                 className="border-2 border-[#00FF00] focus:outline-none bg-black text-white placeholder-gray-400 rounded-lg px-4 py-3 w-full transition"
                 type="email"
-                placeholder="universal+805293@doblexxo.net"
+                placeholder="nea@streaming.com"
                 required
                 value={email}
                 onChange={(event) => setEmail(event.target.value)}
